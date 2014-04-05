b3c771037373f68793e7484a163ac8af48587553
C.W. Betts <computers57@hotmail.com>
2014-04-05 13:34:15 -0600

    Set up the COM interface.

--- 360AudioDriver/360Driver.cpp
+++ 360AudioDriver/360Driver.cpp
@@ -8,6 +8,16 @@
 
 #include "360Driver.h"
 
+Three60Driver::Three60Driver() : RefCount(1)
+{
+	
+}
+
+Three60Driver::~Three60Driver()
+{
+	
+}
+
 HRESULT Three60Driver::QueryInterface(REFIID iid, LPVOID *ppv)
 {
 	return 0;
@@ -15,11 +25,48 @@ HRESULT Three60Driver::QueryInterface(REFIID iid, LPVOID *ppv)
 
 ULONG Three60Driver::AddRef()
 {
-	return 0;
+	return ++RefCount;
 }
 
 ULONG Three60Driver::Release()
 {
-	return 0;
+	RefCount -= 1;
+	if (RefCount == 0) {
+		delete this;
+		return 0;
+	}
+	return RefCount;
 }
 
+HRESULT Three60Driver::SQueryInterface(void *thisPointer, REFIID iid, LPVOID *ppv)
+{
+	return getThis(thisPointer)->QueryInterface(iid, ppv);
+}
+
+ULONG Three60Driver::SAddRef(void *thisPointer)
+{
+	return getThis(thisPointer)->AddRef();
+}
+
+ULONG Three60Driver::SRelease(void *thisPointer)
+{
+	return getThis(thisPointer)->Release();
+}
+
+IOCFPlugInInterface** Three60Driver::Alloc()
+{
+    Three60Driver *me = new Three60Driver;
+    if (!me) {
+        return NULL;
+    }
+    return (IOCFPlugInInterface **)(&me->iIOCFPlugInInterface.pseudoVTable);
+}
+
+extern void* Audio360Factory(CFAllocatorRef allocator, CFUUIDRef typeID)
+{
+    void* theAnswer = NULL;
+    if(CFEqual(typeID, kAudioServerPlugInTypeUUID)) {
+		theAnswer = Three60Driver::Alloc();
+    }
+    return theAnswer;
+}
--- 360AudioDriver/360Driver.h
+++ 360AudioDriver/360Driver.h
@@ -11,14 +11,35 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 #include <CoreFoundation/CFPlugInCOM.h>
+#include <IOKit/IOCFPlugIn.h>
 
 class Three60Driver : IUnknown {
+	void *_reserved;
 	ULONG RefCount;
 	
+	typedef struct _Audio360InterfaceMap
+    {
+        IUnknownVTbl *pseudoVTable;
+        Three60Driver *obj;
+    } Audio360InterfaceMap;
+
+	Audio360InterfaceMap iIOCFPlugInInterface;
+	static inline Three60Driver *getThis (void *oSelf) { return (Three60Driver *) ((Audio360InterfaceMap *) oSelf)->obj; }
+
 public:
+	
+	Three60Driver();
+	virtual ~Three60Driver();
+	
+	static IOCFPlugInInterface** Alloc();
+	
 	virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID *ppv);
     virtual ULONG STDMETHODCALLTYPE AddRef();
     virtual ULONG STDMETHODCALLTYPE Release();
+	
+    static HRESULT SQueryInterface(void *thisPointer, REFIID iid, LPVOID *ppv);
+    static ULONG SAddRef(void *thisPointer);
+    static ULONG SRelease(void *thisPointer);
 };
 
 #endif /* defined(___60_Driver___60Driver__) */
