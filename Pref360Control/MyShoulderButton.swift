e74d7707a3b53ef820c4ce91979a65d112efe50a
C.W. Betts <computers57@hotmail.com>
2014-11-07 15:05:56 -0700

    All of the custom views are now written in Swift.

---
+++ Pref360Control/MyShoulderButton.swift
@@ -0,0 +1,34 @@
+//
+//  MyShoulderButton.swift
+//  360 Driver
+//
+//  Created by C.W. Betts on 11/7/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+import Cocoa
+
+private let INSET_AMOUNT: CGFloat = 2
+
+
+class MyShoulderButton: NSView {
+	var pressed: Bool = false {
+		didSet {
+			self.needsDisplay = true
+		}
+	}
+	
+	override func drawRect(dirtyRect: NSRect) {
+		var area = self.bounds
+		
+		NSDrawLightBezel(area, area);
+		if (pressed) {
+			area.origin.x += INSET_AMOUNT;
+			area.origin.y += INSET_AMOUNT;
+			area.size.width -= INSET_AMOUNT * 2;
+			area.size.height -= INSET_AMOUNT * 2;
+			NSColor.blueColor().set()
+			NSRectFill(area);
+		}
+	}
+}
