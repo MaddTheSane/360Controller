a2215723f229d4ef1e449a51d1deb0fed3cac1bb
C.W. Betts <computers57@hotmail.com>
2015-01-08 14:59:25 -0700

    More work on the menu handler.

--- 360BatteryMenu/BattMenuHandler.m
+++ 360BatteryMenu/BattMenuHandler.m
@@ -8,14 +8,31 @@
 
 #import "BattMenuHandler.h"
 
+#define kShowCondition @"ShowCondition"
+
+@interface BattMenuHandler ()
+@property (strong) NSObject *defaultsObserver;
+@end
+
 @implementation BattMenuHandler
+
++ (void)initialize
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSDictionary *usrDict = @{kShowCondition: @(BMHShowConditionBattOnly)};
+        [[NSUserDefaults standardUserDefaults] registerDefaults:usrDict];
+    });
+}
 
 - (void)buildStatusMenu
 {
     self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
     _statusItem.highlightMode = YES;
     //_statusItem.enabled = NO;
-    _statusItem.title = @"Hello";
+    NSImage *a360Image = [NSImage imageNamed:@"360ControllerTemplate"];
+    [a360Image setTemplate:YES];
+    _statusItem.image = a360Image;
     NSMenu *menu = [[NSMenu alloc] init];
     NSMenuItem *anItem = [[NSMenuItem alloc] initWithTitle:@"Hello" action:@selector(aQuit:) keyEquivalent:@""];
     anItem.target = self;
@@ -26,9 +43,42 @@
 - (instancetype)init
 {
     if (self = [super init]) {
-        [self buildStatusMenu];
+        self.defaultsObserver =
+        [[NSNotificationCenter defaultCenter]
+         addObserverForName:NSUserDefaultsDidChangeNotification
+         object:nil queue:[NSOperationQueue mainQueue]
+         usingBlock:^(NSNotification *note) {
+             NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+             self.showCondition = [defaults integerForKey:kShowCondition];
+             switch (self.showCondition) {
+                 case BMHShowConditionAlways:
+                 case BMHShowConditionBattOnly:
+                     if (!self.statusItem) {
+                         [self buildStatusMenu];
+                     }
+                     break;
+                     
+                 case BMHShowConditionNone:
+                     self.statusItem = nil;
+                     break;
+                     
+                 default:
+                     break;
+             }
+         }];
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        self.showCondition = [defaults integerForKey:kShowCondition];
+        if (_showCondition != BMHShowConditionNone) {
+            [self buildStatusMenu];
+        }
     }
     return self;
+}
+
+- (void)dealloc
+{
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center removeObserver:self.defaultsObserver];
 }
 
 - (IBAction)aQuit:(id)sender
